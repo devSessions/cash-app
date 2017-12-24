8aed6036fe056f0f0fc574b6272599b8729207ae
Rabin Gaire <rabingaire20@gmail.com>
2017-12-24 18:42:37 +0545

    uploaded image to cloudinary server

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,6 +7,7 @@ import {
 } from 'react-native';
 import Camera from 'react-native-camera';
 import axios from 'axios';
+import CryptoJS from 'crypto-js';
 
 import InfoBar from './common/InfoBar';
 import Button from './common/Button';
@@ -37,16 +38,36 @@ export default class Main extends Component {
   }
 
   sendToServer(image) {
-    this.setState({ status: 'ERROR', cash: '404', backgroundColor: '#D81159' });
-    // axios.post('/test', {
-    //   data: image,
-    // })
-    // .then((response) => {
-    //   this.setState({ status: 'DETECTED', cash: 'RS. 1000', backgroundColor: '#41D3BD' });
-    // })
-    // .catch((error) => {
-    //   this.setState({ status: 'ERROR', cash: error, backgroundColor: '#D81159' });
-    // });
+    if(image.path) {
+      let timestamp = (Date.now() / 1000 | 0).toString();
+      let apiKey = '193473961772195';
+      let apiSecret = 'ROHw1Q_mHPEO0uL7l7kgMmZcv0c';
+      let cloud = 'rabingaire';
+      let hashString = `timestamp=${timestamp}${apiSecret}`;
+      let signature = CryptoJS.SHA1(hashString).toString();
+      let uploadUrl = `https://api.cloudinary.com/v1_1/${cloud}/image/upload`;
+
+      let formdata = new FormData();
+      formdata.append('file', {uri: image.path, type: 'image/jpg', name: 'upload.jpg'});
+      formdata.append('timestamp', timestamp);
+      formdata.append('api_key', apiKey);
+      formdata.append('signature', signature);
+
+      let uploadCloudinary = new Promise((resolve, reject) => {
+        let xhr = new XMLHttpRequest();
+        xhr.open('POST', uploadUrl);
+        xhr.onload = () => resolve(xhr.responseText);
+        xhr.onerror = () => reject(xhr.statusText);
+        xhr.send(formdata);
+      });
+
+      uploadCloudinary.then((resolve) => {
+        this.setState({ status: 'DETECTED', cash: 'RS. 1000', backgroundColor: '#41D3BD' });
+      })
+      .catch((error) => {
+        this.setState({ status: 'ERROR', cash: '400', backgroundColor: '#D81159' });
+      });
+    }
   }
 
   render() {
